7c4ed49e82b80acf7affaf900db166d213c93638
Abiodun Osagie <abiodunosagie8@gmail>
2025-03-13 16:43:23 +0100

    Custom Navigation Bar with logo animaiton.

---
+++ TouchDown/View/Home/NavigationBarView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBarView.swift
+//  TouchDown
+//
+//  Created by Abiodun Osagie on 13/03/2025.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}

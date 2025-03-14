02a27975b227476ccf22334ac7666e103c649740
Abiodun Osagie <abiodunosagie8@gmail>
2025-03-14 17:26:07 +0100

    Brands section

---
+++ TouchDown/Model/BrandModel.swift
@@ -0,0 +1,8 @@
+//
+//  BrandModel.swift
+//  TouchDown
+//
+//  Created by Abiodun Osagie on 14/03/2025.
+//
+
+import Foundation
---
+++ TouchDown/View/Home/BrandGridView.swift
@@ -0,0 +1,18 @@
+//
+//  BrandGridView.swift
+//  TouchDown
+//
+//  Created by Abiodun Osagie on 14/03/2025.
+//
+
+import SwiftUI
+
+struct BrandGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrandGridView()
+}

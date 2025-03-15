5039ea469492ad08c8cb3a3169d621dc2a6f010a
Abiodun Osagie <abiodunosagie8@gmail>
2025-03-15 11:33:44 +0100

    Details view top part

---
+++ TouchDown/App/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  TouchDown
+//
+//  Created by Abiodun Osagie on 14/03/2025.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
---
+++ TouchDown/View/Detail/TopPartDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TopPartDetailView.swift
+//  TouchDown
+//
+//  Created by Abiodun Osagie on 15/03/2025.
+//
+
+import SwiftUI
+
+struct TopPartDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopPartDetailView()
+}
